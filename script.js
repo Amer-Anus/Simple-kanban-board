871bc661dbf4ab831968b4c1d04db418f14bb75b
Amer Thander <amerthander@gmail.com>
2026-09-10 23:00:10 +0530

    formated document

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 const cards = document.querySelectorAll('.card');
 const lists = document.querySelectorAll('.list');
 
-for(const card of cards){
+for (const card of cards) {
     card.addEventListener('dragstart', dragStart);
     card.addEventListener('dragend', dragEnd);
 }
 
-for(const list of lists){
+for (const list of lists) {
     list.addEventListener('dragover', dragOver);
     list.addEventListener('dragenter', dragEnter);
     list.addEventListener('dragleave', dragLeave);
     list.addEventListener('drop', dragDrop);
 }
 
-function dragStart(e){
+function dragStart(e) {
 
-     e.dataTransfer.setData('text/plain', e.target.id);
+    e.dataTransfer.setData('text/plain', e.target.id);
 }
 
-function dragEnd(e){
+function dragEnd(e) {
     console.log('Drag End');
 }
 
-function dragOver(e){
+function dragOver(e) {
     e.preventDefault();
     console.log('Drag Over');
 }
 
-function dragEnter(e){
+function dragEnter(e) {
     e.preventDefault();
-    
+
     this.classList.add('over');
 }
 
-function dragLeave(e){
+function dragLeave(e) {
     e.preventDefault();
-    
+
     this.classList.remove('over');
 }
 
-function dragDrop(e){
+function dragDrop(e) {
     const id = e.dataTransfer.getData('text/plain');
     const card = document.getElementById(id);
     this.appendChild(card);
